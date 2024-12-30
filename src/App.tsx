8bb078867305095b333081503c47ffbf4c6651f0
ahadsts9901 <ahadsts990@gmail.com>
2024-12-31 01:13:39 +0500

    features

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react"
 import "./App.css"
 
 const App = () => {
-
-    const [teams, set_teams] = useState([])
-
     return (
-        <div>
-            {/* <form onSubmit={addTeam}></form> */}
-        </div>
+        <div>App</div>
     )
 }
 
