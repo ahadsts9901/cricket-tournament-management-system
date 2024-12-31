152b8916e12ec34352da0ac727eb47652083692c
ahadsts9901 <ahadsts990@gmail.com>
2024-12-31 22:34:49 +0500

    standings

--- src/components/Fixtures.tsx
+++ src/components/Fixtures.tsx
@@ -60,17 +60,17 @@ export const SingleMatch = ({ match, index, state, set_state }: { match: Match, 
             <div className="flex justify-start items-center gap-2">
                 <p className="index w-[65px] text-left text-purple-800">{`${index + 1})`}</p>
                 <TextField style={{ width: "80px" }} placeholder="Runs"
-                    className="index"
+                    className="index" type="number"
                     value={full_match.team1.runs}
                     onChange={(e: any) => set_full_match({ ...full_match, team1: { ...full_match.team1, runs: +e.target.value } })}
                 />
                 <TextField style={{ width: "80px" }} placeholder="Wickets"
-                    className="index"
+                    className="index" type="number"
                     value={full_match.team1.wickets}
                     onChange={(e: any) => set_full_match({ ...full_match, team1: { ...full_match.team1, wickets: +e.target.value } })}
                 />
                 <TextField style={{ width: "80px" }} placeholder="Overs"
-                    className="index"
+                    className="index" type="number"
                     value={full_match.team1.overs}
                     onChange={(e: any) => set_full_match({ ...full_match, team1: { ...full_match.team1, overs: +e.target.value } })}
                 />
@@ -78,17 +78,17 @@ export const SingleMatch = ({ match, index, state, set_state }: { match: Match, 
             <p className="capitalize text-purple-900">{match.team1.teamName} v {match.team2.teamName}</p>
             <div className="flex justify-end items-center gap-2">
                 <TextField style={{ width: "80px" }} placeholder="Runs"
-                    className="index"
+                    className="index" type="number"
                     value={full_match.team2.runs}
-                    onChange={(e: any) => set_full_match({ ...full_match, team2: { ...full_match.team1, runs: +e.target.value } })}
+                    onChange={(e: any) => set_full_match({ ...full_match, team2: { ...full_match.team2, runs: +e.target.value } })}
                 />
                 <TextField style={{ width: "80px" }} placeholder="Wickets"
-                    className="index"
+                    className="index" type="number"
                     value={full_match.team2.wickets}
                     onChange={(e: any) => set_full_match({ ...full_match, team2: { ...full_match.team2, wickets: +e.target.value } })}
                 />
                 <TextField style={{ width: "80px" }} placeholder="Overs"
-                    className="index"
+                    className="index" type="number"
                     value={full_match.team2.overs}
                     onChange={(e: any) => set_full_match({ ...full_match, team2: { ...full_match.team2, overs: +e.target.value } })}
                 />
--- src/components/Standings.tsx
+++ src/components/Standings.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { Match, Team } from "../types";
 
 const Standings = ({ state, set_state }: any) => {
@@ -15,30 +16,35 @@ const Standings = ({ state, set_state }: any) => {
                 totalRuns: 0,
                 totalOvers: 0,
                 netRunRate: 0,
+                points: 0,
             };
 
             state.matches.forEach((match: Match) => {
-                if (match.team1.id === team.id || match.team2.id === team.id) {
+                if ((match.team1.id === team.id && match.team1.runs) || (match.team2.id === team.id && match.team2.runs)) {
                     teamStats.matchesPlayed += 1;
                     if (match.team1.id === team.id) {
                         teamStats.totalRuns += match.team1.runs;
                         teamStats.totalOvers += match.team1.overs;
                         if (match.team1.runs > match.team2.runs) {
                             teamStats.wins += 1;
+                            teamStats.points += 2;
                         } else if (match.team1.runs < match.team2.runs) {
                             teamStats.losses += 1;
                         } else {
                             teamStats.ties += 1;
+                            teamStats.points += 1;
                         }
                     } else {
                         teamStats.totalRuns += match.team2.runs;
                         teamStats.totalOvers += match.team2.overs;
                         if (match.team2.runs > match.team1.runs) {
                             teamStats.wins += 1;
+                            teamStats.points += 2;
                         } else if (match.team2.runs < match.team1.runs) {
                             teamStats.losses += 1;
                         } else {
                             teamStats.ties += 1;
+                            teamStats.points += 1;
                         }
                     }
                 }
@@ -73,7 +79,16 @@ const Standings = ({ state, set_state }: any) => {
     }
 
     return (
-        <div>Standings</div>
+        <div className="w-full flex flex-col gap-2">
+            <div className="w-full flex justify-between items-center flex-wrap gap-2 mb-2">
+                <h2 className="text-left uppercase tracking-[4px] text-purple-700 text-2xl mb-2 mr-4">Fixtures</h2>
+                <div className="flex justify-start items-center gap-2 flex-wrap">
+                    <Button className="w-[180px]" color="primary" variant="outlined"
+                        onClick={update_standings}
+                    >Update Standings</Button>
+                </div>
+            </div>
+        </div>
     )
 }
 
