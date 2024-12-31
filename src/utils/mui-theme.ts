f1421f8e6730b06b860488122a27c47a9c4a984f
ahadsts9901 <ahadsts990@gmail.com>
2024-12-31 12:54:48 +0500

    code

--- src/utils/mui-theme.ts
+++ src/utils/mui-theme.ts
@@ -3,7 +3,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from '@mui/system';
 
 export const themeData: any = {
     palette: {
