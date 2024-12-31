356dee77a57e1fd8475ce23afa6c92927fa972ec
ahadsts9901 <ahadsts990@gmail.com>
2024-12-31 21:59:47 +0500

    code

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,12 @@
-import { FormEvent, useState } from "react"
-import { Team } from "../types"
+import { FormEvent, Fragment, useState } from "react"
+import { State, Team } from "../types"
 import { Button, TextField } from "@mui/material"
+import ConfirmAlertMUI from "./ConfirmAlertMUI"
 
 export const Form = ({ state, set_state }: any) => {
     const [team, set_team] = useState("")
+    const [is_alert_open, set_is_alert_open] = useState(false)
+    const [alert_data, set_alert_data] = useState<any>({})
 
     const addTeam = (e: FormEvent) => {
         e.preventDefault()
@@ -18,16 +21,44 @@ export const Form = ({ state, set_state }: any) => {
         set_team("")
     }
 
+    const reset_app = () => {
+        const newState: State = {
+            matches: [],
+            teams: [],
+        }
+        set_state(newState)
+        localStorage.setItem("state", JSON.stringify(newState))
+        set_is_alert_open(false)
+    }
+
+    const confirmResetApp = () => {
+        set_alert_data({
+            title: "Reset all data",
+            description: "Are you sure you want to delete all the data in the app?. The action cannot be undone",
+            fun: reset_app
+        })
+        set_is_alert_open(true)
+    }
+
     return (
-        <form className="w-full flex gap-4 flex-wrap justify-end p-2" onSubmit={addTeam}>
-            <TextField
-                fullWidth
-                onChange={(e: any) => set_team(e.target.value)}
-                value={team}
-                placeholder="Enter team name"
+        <Fragment>
+            <ConfirmAlertMUI
+                open={is_alert_open}
+                setOpen={set_is_alert_open}
+                title={alert_data?.title}
+                description={alert_data?.description}
+                fun={alert_data?.fun}
             />
-            <Button className="w-[120px]" color="primary" variant="outlined">Add Team</Button>
-            <Button className="w-[120px]" color="primary" variant="outlined">Reset</Button>
-        </form>
+            <form className="w-full flex gap-4 flex-wrap justify-end p-2" onSubmit={addTeam}>
+                <TextField
+                    fullWidth
+                    onChange={(e: any) => set_team(e.target.value)}
+                    value={team}
+                    placeholder="Enter team name"
+                />
+                <Button className="w-[120px]" color="primary" variant="outlined" type="submit">Add Team</Button>
+                <Button className="w-[120px]" color="primary" variant="outlined" onClick={confirmResetApp}>Reset</Button>
+            </form>
+        </Fragment>
     )
 }
