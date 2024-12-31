cfc21829fabe53f25caed9d3212838bcb3c1ee7c
ahadsts9901 <ahadsts990@gmail.com>
2024-12-31 13:20:34 +0500

    code

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import "./App.css"
 
 const App = () => {
     return (
-        <div className="w-full">
-            <p className="bg-red-700">hi'</p>
-        </div>
+        <div>App</div>
     )
 }
 
